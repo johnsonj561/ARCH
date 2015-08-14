8123b411c5de490b5d76be3a7abafde9a9f2375d
Justin Johnson <johnsonj561@gmail.com>
2015-08-14 18:11:57 -0400

    Content Update

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="ARCH of Florida Halfway Houses - Providing affordable, safe, and structured sober living environments
-                                      to adults recovering from addiction and alcoholism in West Palm Beach Florida">
+    <meta name="description" content="ARCH of Florida Halfway Houses - Providing affordable sober living environments
+                                      to adults recovering from addiction and alcoholism in Palm Beach County">
     <meta name="author" content="ARCH Florida">
     <meta name="ROBOTS" content="INDEX, FOLLOW">
     <link rel="icon" type="image/x-icon" href="img/house-favicon.png"/>
 
-    <title>A.R.C.H. of Florida | West Palm Beach</title>
+    <title>A.R.C.H. | West Palm Beach</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -23,8 +22,6 @@
     <link href="css/custom.css" rel="stylesheet">
     <?php require_once('templates/facebook-google-plugin.html'); ?>
   </head>
-
-
   <body>
     <!-- Navigation -->
     <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
@@ -37,7 +34,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand topnav" href="index.php">ARCH of Florida Housing</a>
+          <a class="navbar-brand topnav" href="index.php">ARCH Florida Housing</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -47,9 +44,6 @@
             </li>
             <li>
               <a href="/view/gallery.php">Photos</a>
-            </li>
-            <li>
-              <a href="/view/arch-advantages.php">Services</a>
             </li>
             <li>
               <a href="view/meetings.php">Meetings</a>
@@ -70,16 +64,12 @@
         <div class="row">
           <div class="col-lg-12">
             <div class="intro-message">
-          <!--    <h2>RECOVERY STARTS HERE</h2>   -->
-           <!--   <hr class="intro-divider">   -->
+              <h2>ARCH Halfway Houses Provide Safe, Structured Sober Living Environments 
+                to Adults Recovering From Drug and Alcohol Addiction in Palm Beach County.</h2>
               <ul class="list-inline intro-social-buttons">
                 <li>
                   <a href="view/gallery.php" class="btn btn-default btn-lg grey-blue">
                     <span class="network-name">View Photos</span></a>
-                </li>
-                <li>
-                  <a href="/view/arch-advantages.php" class="btn btn-default btn-lg grey-blue">
-                    <span class="network-name">ARCH Advantages</span></a>
                 </li>
                 <li>
                   <a href="view/meetings.php" class="btn btn-default btn-lg grey-blue">                                   
@@ -90,14 +80,7 @@
                     <span class="network-name">Get Help Now</span></a>
                 </li>
               </ul>
-              <h2>ARCH Halfway Houses offer affordable, safe, strongly structured living environments 
-                to adults recovering from substance abuse dependencies in West Palm Beach, Florida.</h2>
-              <ul class="list-inline intro-social-buttons">
-                <li>
-                  <a href="#about" class="btn btn-default btn-lg grey-blue">
-                    <span class="network-name">Read More</span></a>
-                </li>
-              </ul>
+              <img src="img/arch-quote.png" class="img-responsive arch-quote" alt="Alcoholics Anonymous Recovery Quote"/>
             </div>
           </div>
         </div>
@@ -107,124 +90,159 @@
     <!-- Page Content -->
 
     <a name="about"></a>
-    <div class="content-section-a">
 
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-5 col-sm-6">
-            <hr class="section-heading-spacer">
-            <div class="clearfix"></div>
-            <h2 class="section-heading">About ARCH of Florida</h2>
-            <p class="lead">ARCH halfway houses were designed to give the recovering alcoholic and addict the best possible 
-              opportunity to re-create their lives. ARCH is a non-profit orginazation led by men and women with a true 
-              passion for helping others to recover from their addiction. We understand how extremely difficult it can be to 
-              start over after losing everything to this baffling disease, and we also understand what tools and resources will 
-              be most helpful for a successful recovery. </p>
-            <p class="lead">Whether looking for the right halfway house for yourself, a friend, or a loved one - 
-              <a href="view/contact.php">Contact ARCH</a> to see how our sober living environment can help lay the foundation 
-              through which we build our triumphant arch to freedom.
-            </p>
-          </div>
-          <div class="col-lg-5 col-lg-offset-2 col-sm-6">
-            <a href="view/gallery.php">
-              <img class="img-responsive" src="img/bedroom-1.jpg" alt="Halfway house bedroom - Sober House"></a>
-            <p class="text-center">Click image to view our property photo gallery</p>
-          </div>
-
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <hr class="section-heading-spacer">
+          <div class="clearfix"></div>
+          <h1 class="section-heading">ARCH Florida Halfway Houses</h1>
+          <p class="lead">ARCH halfway houses provide recovering alcoholics and addicts with the best possible 
+            opportunity to re-create their lives and obtain long term sobriety. Our staff has a real passion for helping others to recover 
+            from their addictions. Through experience we understand how difficult these times can be, and we also understand what tools and resources will be most effective in achieving permanent recovery from drugs and alcohol.</p>
+          <p class="lead">If you or a loved one is struggling with drugs and alcohol or searching for a structured sober living environment in
+            Palm Beach County - <a href="view/contact.php">Contact ARCH Florida today.</a> Our staff is available 24/7 to assist anyone with
+            a drug or alcohol problem. In the case that we do not have any available beds, we will take the time to set you up with an
+            appropriate facility or sober housing community.
+          </p>
         </div>
-        <div class="row">
-          <div class="col-lg-12">
-            <p class="lead">Unlike most Florida halfway houses, ARCH utilizes state funding to grant it's qualified residents up to 12 
-              weeks of rent free living. This window allows residents to get involved in the recovery process and find a job so that 
-              they
-              may return to a healthy, self supporting life style. Our aim is to help the addict who is down and who has lost all, 
-              and we are not here to enable anyone to continue down the path of alcoholic destruction. Being said - ARCH recovery 
-              houses require active
-              participation in 12 Step Programs, intensive work with sponsors and sober supports, and a positive community
-              spirit that reflects their desire to achieve long term sobriety. Staff members have walked this path
-              before you and are here to guide you through this crucial re-construction period.
+        <div class="col-lg-6">
+          <a href="view/gallery.php">
+            <img class="img-responsive home-page-image-1" src="img/spiritual-freedom-from-drugs.jpg" alt="Sobriety - Free From Drugs"></a>
+          <p class="text-center">Click image to view ARCH Florida Photo Gallery</p>
+        </div>
+
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <p class="lead">ARCH utilizes state funding to grant qualified residents up to 12 
+            weeks of rent free living. This window allows residents to get their feet planted firmly in the recovery process, develop a
+            solid sober network, and secure a job that will allow them to return to a healthy, self supporting life style. Our aim is to help 
+            the addict who is down and who has lost all, and we are not here to enable anyone to continue down the path of 
+            alcoholic destruction. Being said - ARCH recovery houses require active
+            participation in 12 Step Programs, intensive work with sponsors and sober supports, and a positive community
+            spirit that reflects their desire to achieve long term sobriety. Staff members have walked this path
+            before you and are here to guide you every step of the way. To see if you or your loved one qualifies for this service,
+            <a href="http://www.pbcgov.com/communityservices/programs/humanservices/homeless/homelessservices.htm">contact the West
+              Palm Beach Lewis Center.</a> 
+          </p>
+          <p class="lead"></p>
+        </div>
+      </div>
+    </div>
+
+    <br><br>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <img class="img-responsive home-page-image-1 pull-left" src="img/adults-doing-stepwork.jpg" 
+               alt="Halfway House Enforces Stepwork and 12 Step Program">
+        </div>
+        <div class="col-lg-6">
+          <hr class="section-heading-spacer">
+          <div class="clearfix"></div>
+          <h1 class="section-heading">Structured Sober Living</h1>
+          <p class="lead">The first year of the recovery process is an extremely difficult time for any individual striving to overcome
+            drugs and alcohol and achieve sobriety. Spending 3 - 6 months in a structured sober living environment that supports and
+            encourages the recovery process in all aspects will greatly reduce the risk of a relapse. Providing residents with healthy rules
+            and guidelines to live by will bring about the necessary life style changes that promote permanent sobriety. Staff supervision
+            of the recovery process will keep residents on the right track and develop a positive, recovery oriented community that 
+            will benefit all individuals involved. Being a strongly structured and supervised sober living environment, any behaviors
+            that contradict our vision of sobriety will not be tolerated.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <br><br>
+
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 col-sm-6">
+          <hr class="section-heading-spacer">
+          <div class="clearfix"></div>
+          <h1 class="section-heading">"We absolutely insist on enjoying life"
+            <br><small>~Bill Wilson, Alcoholics Anonymous</small></h1>
+          <p class="lead">Just as important to an effective program of recovery is the fellowship that develops around
+            us as we begin our journey to freedom. We at ARCH work to bring our residents together to participate in regular
+            events and activities. <a href="view/contact.php">Send us a message</a> and see what events are being held this month!
+          <ul>
+            <li>Fishing &amp; Snorkeling Outings</li>
+            <li>Local 12 Step Fellowship Events</li>
+            <li>Camping Trips</li>
+            <li>Movie and Game Nights</li>
+            <li>Sporting Activities &amp; Competitions</li>
+            <li>Community BBQs and Cookouts</li>
+          </ul>
+          </p>
+      </div>
+      <div class="col-lg-5 col-lg-offset-2 col-sm-6">
+        <img class="img-responsive home-page-image-1" src="img/people-having-fun-in-sobriety.jpg" 
+             alt="Halfway House Residents Having Fun In Sobriety">
+      </div>
+    </div>
+    <hr>
+    <div class="row"><a href="#top">Back to Top</a><br></div>
+    </div>
+  <!-- SOCIAL MEDIA LINKS -->
+  <div class="container social-media-container">
+    <row class="text-center">
+      <ul class="share-buttons">
+        <li><a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.archrecoveryhouses.com&t=ARCH%20Florida%20Halfway%20Houses"
+               title="Share on Facebook" target="_blank"><img src="../img/Facebook.png" alt="Facebook Recovery"></a></li>
+        <li><a href="https://twitter.com/intent/tweet?source=http%3A%2F%2Fwww.archrecoveryhouses.com&text=ARCH%20Florida%20Halfway%20Houses:%20http%3A%2F%2Fwww.archrecoveryhouses.com"
+               target="_blank" title="Tweet"><img src="../img/Twitter.png" alt="Twitter Sobriety"></a></li>
+        <li><a href="https://plus.google.com/share?url=http%3A%2F%2Fwww.archrecoveryhouses.com" target="_blank" title="Share on Google+">
+          <img src="../img/Google+.png" alt="Google+ Recovery"></a></li>
+        <li><a href="http://pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.archrecoveryhouses.com&description=Providing%20structured%20living%20environments%20to%20adults%20recovering%20from%20drug%20or%20alcohol%20addiction%20in%20Palm%20Beach%20County." 
+               target="_blank" title="Pin it"><img src="../img/Pinterest.png" alt="Pinterest Sobriety"></a></li>
+        <li><a href="mailto: ?subject=ARCH%20Florida%20Halfway%20Houses&body=Providing%20structured%20living%20environments%20to%20adults%20recovering%20from%20drug%20or%20alcohol%20addiction%20in%20Palm%20Beach%20County.:%20http%3A%2F%2Fwww.archrecoveryhouses.com" 
+               target="_blank" title="Email"><img src="../img/Email.png" alt="Email Halfway House"></a></li>
+      </ul>
+    </row>
+  </div>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="col-lg-6">
+            <ul class="list-inline">
+              <li>
+                <a href="#top">Home</a>
+              </li>
+              <li class="footer-menu-divider">&sdot;</li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li class="footer-menu-divider">&sdot;</li>
+              <li>
+                <a href="view/gallery.php">Photos</a>
+              </li>
+              <li class="footer-menu-divider">&sdot;</li>
+
+              <li>
+                <a href="view/contact.php">Contact</a>
+              </li>
+            </ul>
+            <p class="copyright text-muted small">Copyright &copy; ARCH Florida Recovery 2015. All Rights Reserved</p>
+          </div>
+          <div class="col-lg-6">
+            <p class="copyright text-muted medium pull-right">
+              <img src="img/kb-logo-v3-75x60.png" alt="KB Software Solutions Logo"/>
+              <a href="http://www.kbsoftwaresolutions.com">Website Development by KB Software Solutions</a>
             </p>
           </div>
         </div>
       </div>
     </div>
-    <!-- /.content-section-a -->
+  </footer>
+  <!-- jQuery -->
+  <script src="js/jquery.js"></script>
 
-    <!-- .content-section-b -->
-    <div class="content-section-b">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-5 col-sm-6">
-            <hr class="section-heading-spacer">
-            <div class="clearfix"></div>
-            <h2 class="section-heading">"We absolutely insist on enjoying life"
-              <small>~Bill Wilson, Big Book of Alcoholics Anonymous</small></h2>
-            <p class="lead">Just as important to an effective program of recovery is the fellowship that develops around
-              us as we begin our journey to freedom. We at ARCH work to bring the our residents together to particpate in regular
-              activities and events. For after all, we got sober to escape the misery and terror of addiction, not to lead boring
-              lives!
-            </p>
-            <p class="lead">If you are interested in learning more about ARCH activities and events, 
-              <a href="view/arch-advantages.php">check out our ARCH Advantages page</a> or 
-              <a href="view/contact.php">send us a message</a> to find out what our community has planned for next month!</p>
-          </div>
-          <div class="col-lg-5 col-lg-offset-2 col-sm-6">
-            <img class="img-responsive" src="img/people-having-fun-in-sobriety.jpg" 
-                 alt="Halfway House Residents Having Fun In Sobriety">
-          </div>
-        </div>
-        <div class="row"><a href="#top">Back to Top</a><br></div>
-      </div>
-    </div>
-
-
-    <!-- Footer -->
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="col-lg-10">
-              <ul class="list-inline">
-                <li>
-                  <a href="index.php">Home</a>
-                </li>
-                <li class="footer-menu-divider">&sdot;</li>
-                <li>
-                  <a href="#about">About</a>
-                </li>
-                <li class="footer-menu-divider">&sdot;</li>
-                <li>
-                  <a href="/view/gallery.php">Photos</a>
-                </li>
-                <li class="footer-menu-divider">&sdot;</li>
-                <li>
-                  <a href="/view/arch-advantages.php">Services</a>
-                </li>
-                <li class="footer-menu-divider">&sdot;</li>
-                <li>
-                  <a href="/view/contact.php">Contact</a>
-                </li>
-              </ul>
-              <p class="copyright text-muted small">Copyright &copy; ARCH of Florida 2015. All Rights Reserved</p>
-              <p class="copyright text-muted medium">
-                <a href="http://www.sharpcutlawns.com/blog">Website Development by KB Designs</a>
-              </p>
-            </div>
-            
-            <!-- facebook and google share buttons -->
-            <div class="col-lg-2">
-              <?php require_once('templates/facebook-google-share-buttons.html'); ?>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+  <!-- Bootstrap Core JavaScript -->
+  <script src="js/bootstrap.min.js"></script>
 
   </body>
 
